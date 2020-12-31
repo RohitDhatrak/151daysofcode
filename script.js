8c7cfae4695890f654422decb4c4055177ec3ce6
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-31 20:16:51 +0000

    fix: hrefs

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 let tweet = document.querySelector('#tweet');
 let tweet2 = document.querySelector('#tweet2');
-tweet.addEventListener('click', function() {
-    let message = `I'm publicly committing to the 151daysofcode challenge by @neogcamp. Join and learn with me 👇
-    https://151daysofcode.netlify.app/`;
-    
-    let twitterUrl = `https://twitter.com/intent/tweet?text=${message}`;
-    window.open(twitterUrl, '_blank');
+tweet.addEventListener("click", function () {
+  let message = `I'm publicly committing to the #151daysofcode challenge by @neogcamp.
+
+Join and learn with me 👇
+https://151daysofcode.netlify.app/
+
+#neogcamp #151daysofcode`;
+
+  let finalMessage = encodeURIComponent(message);
+
+  let twitterUrl = `https://twitter.com/intent/tweet?text=${finalMessage}`;
+  window.open(twitterUrl, "_blank");
 });
 
-tweet2.addEventListener('click', function() {
-    let message = `I'm publicly committing to the 151daysofcode challenge by @neogcamp. Join and learn with me 👇
-    https://151daysofcode.netlify.app/`;
-    
-    let twitterUrl = `https://twitter.com/intent/tweet?text=${message}`;
-    window.open(twitterUrl, '_blank');
+tweet2.addEventListener("click", function () {
+  let message = `I'm publicly committing to the #151daysofcode challenge by @neogcamp.
+
+Join and learn with me 👇
+https://151daysofcode.netlify.app/
+
+#neogcamp #151daysofcode`;
+
+  let finalMessage = encodeURIComponent(message);
+
+  let twitterUrl = `https://twitter.com/intent/tweet?text=${finalMessage}`;
+  window.open(twitterUrl, "_blank");
 });
 
 // I'm publicly committing to the &#x23;151daysofcode challenge by @neogcamp.
